f33efdc66ca4debe39b55f62dcc16c0c0253842d
Damion McLoone <damion.mcloone@gmail.com>
2023-02-03 22:02:58 -0600

    Add a dynamically sized grid box

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log('test');
+const size = 8;
+const container = document.querySelector('#container');
+
+container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
+for(let i = 0; i < size * size; i++){
+    const square = document.createElement('div');
+    square.classList.add('square');
+    container.appendChild(square);
+}
+
+const gridElements = document.querySelectorAll('.square');
+
+gridElements.forEach(gridElements => gridElements.addEventListener('click', changecolor));
+
+function changecolor(){
+    square.setAtribute('style', 'background: black;');
+}
