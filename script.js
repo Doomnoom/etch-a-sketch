618e195a6e7abde99fdbe74890fe02b0dfff89b9
Damion McLoone <damion.mcloone@gmail.com>
2023-02-11 20:40:11 -0600

    Add palette tool

--- script.js
+++ script.js
@@ -4,16 +4,21 @@ const container = document.querySelector('#container');
 container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
 container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
+//populates webpage with drawing grid
+
 for(let i = 0; i < size * size; i++){
     const square = document.createElement('div');
     square.classList.add('square');
     container.appendChild(square);
 }
 
+const color = document.querySelector('.colorPicker');
+
 const gridElements = document.querySelectorAll('.square');
 
 gridElements.forEach(gridElements => gridElements.addEventListener('mouseover', changeColor));
-gridElements.forEach(gridElements => gridElements.addEventListener('mouseout', revertColor))
+gridElements.forEach(gridElements => gridElements.addEventListener('mouseout', revertColor));
+gridElements.forEach(gridElements => gridElements.addEventListener('click', test));
 
 function changeColor(){
     this.classList.add('hover');
@@ -21,4 +26,8 @@ function changeColor(){
 
 function revertColor(){
     this.classList.remove('hover');
+}
+
+function test(){
+    this.style.backgroundColor = color.value;
 }
